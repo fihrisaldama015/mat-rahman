542bdf53f349f09383de43dcf89ff2ef6e8dfb6f
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2024-04-29 21:35:57 +0700

    [ADD] soal multi + detect akar

--- app/simulasi/perkalian_suku/Playground.jsx
+++ app/simulasi/perkalian_suku/Playground.jsx
@@ -1,50 +1,37 @@
 "use client";
 import KotakSoal from "@/app/components/KotakSoal";
 import LokasiKotak from "@/app/components/LokasiKotak";
-import update from "immutability-helper";
-import { useEffect, useRef, useState } from "react";
-import { useDrop } from "react-dnd";
 import { snapToGrid } from "@/app/components/snapToGrid";
-import Latex from "react-latex-next";
-import useScreenSize from "@/app/utils/useScreenSize";
 import {
   getEquation,
   getEquationPenjabaran,
-  getPenjabaran,
 } from "@/app/simulasi/perkalian_suku/getEquation";
+import update from "immutability-helper";
+import { useEffect, useRef, useState } from "react";
+import { useDrop } from "react-dnd";
+import Latex from "react-latex-next";
 
 const styles = {
   width: "50%",
   height: "100%",
-  // border: "1px solid black",
   position: "relative",
 };
-
-const NORMAL_BOX_WIDTH = 64;
-const BIG_BOX_WIDTH = 2 * NORMAL_BOX_WIDTH;
-const BOX_TOP = 192;
-const GAP = 32;
-const TOTAL_STEP = 13;
-const SOAL = "x^2 + 2x + 1";
+const LIST_SOAL = ["x^2 + 2x + 1", "x^2 + 3x + 2"];
 
 const Playground = ({ isSnapToGrid }) => {
-  const [step, setStep] = useState(0);
-  const [preview, setPreview] = useState(false);
-  const [previewPL, setPreviewPL] = useState(false);
+  const [soal, setSoal] = useState(LIST_SOAL[0]);
+  const [noSoal, setNoSoal] = useState(1);
   const [result, setResult] = useState({});
-  const [previewHasil, setPreviewHasil] = useState({
-    display: false,
-    step: 0,
-  });
   const [kotak, setKotak] = useState({});
   const [panjang, setPanjang] = useState("");
   const [lebar, setLebar] = useState("");
   const [bentuk, setBentuk] = useState("");
   const [akar_1, setAkar_1] = useState("");
   const [akar_2, setAkar_2] = useState("");
-  const [isBentukBenar, setIsBentukBenar] = useState(false);
-  const [isUkuranBenar, setIsUkuranBenar] = useState(false);
-  const [isAkarBenar, setIsAkarBenar] = useState(false);
+  const [statusJawaban, setStatusJawaban] = useState({
+    type: "",
+    isBenar: false,
+  });
   const [showEmoticon, setShowEmoticon] = useState(false);
   const [showTryAgain, setShowTryAgain] = useState(false);
   const listKotak = {
@@ -55,8 +42,10 @@ const Playground = ({ isSnapToGrid }) => {
     e: { title: "-1", left: 112 + 128 + 64 + 64 + 48, top: 128 },
     f: { title: "X ", left: 112 + 128 + 64 + 64 + 64 + 64, top: 128 },
   };
-  let audioHappy = new Audio("/audio/yay.mp3");
-  let audioSad = new Audio("/audio/sad.mp3");
+  let audioHappy =
+    typeof Audio !== "undefined" ? new Audio("/audio/yay.mp3") : undefined;
+  let audioSad =
+    typeof Audio !== "undefined" ? new Audio("/audio/sad.mp3") : undefined;
 
   const checkAnswer = () => {
     if (
@@ -65,13 +54,19 @@ const Playground = ({ isSnapToGrid }) => {
         result.EqX2,
         result.EqConst,
         result.EqConst2
-      ) == SOAL
+      ) == soal
     ) {
-      setIsBentukBenar(true);
+      setStatusJawaban({
+        type: "bentuk",
+        isBenar: true,
+      });
       setShowEmoticon(true);
       audioHappy.play();
     } else {
-      setIsBentukBenar(false);
+      setStatusJawaban({
+        type: "bentuk",
+        isBenar: false,
+      });
       setShowEmoticon(true);
       audioSad.play();
       setShowTryAgain(true);
@@ -85,14 +80,44 @@ const Playground = ({ isSnapToGrid }) => {
     const lebarString = `${result.EqX2 > 1 ? result.EqX2 : ""}x+${
       result.EqConst2
     }`;
-    if (panjangString == panjang && lebarString == lebar) {
-      setIsUkuranBenar(true);
+    const himpunanPenyelesaian1 = `${
+      result.EqConst > 0 ? "-" + result.EqConst : result.EqConst.substring(1)
+    }`;
+    const himpunanPenyelesaian2 = `${
+      result.EqConst2 > 0 ? "-" + result.EqConst2 : result.EqConst2.substring(1)
+    }`;
+    console.log(himpunanPenyelesaian1);
+    console.log(himpunanPenyelesaian2);
+    if (
+      panjangString == panjang.toLowerCase() &&
+      lebarString == lebar.toLowerCase()
+    ) {
+      if (
+        himpunanPenyelesaian1 == akar_1.toLowerCase() &&
+        himpunanPenyelesaian2 == akar_2.toLowerCase()
+      ) {
+        setStatusJawaban({
+          type: "akar",
+          isBenar: true,
+        });
+        audioHappy.play();
+      } else {
+        setStatusJawaban({
+          type: "akar",
+          isBenar: false,
+        });
+        audioSad.play();
+        setShowTryAgain(true);
+      }
       setShowEmoticon(true);
-      audioHappy.play();
     } else {
-      setIsUkuranBenar(false);
+      setStatusJawaban({
+        type: "ukuran",
+        isBenar: false,
+      });
       setShowEmoticon(true);
       audioSad.play();
+      setShowTryAgain(true);
     }
   };
 
@@ -106,14 +131,52 @@ const Playground = ({ isSnapToGrid }) => {
 
   const tryAgain = () => {
     setShowEmoticon(false);
-    setKotak({});
+    if (statusJawaban.type == "bentuk") {
+      setKotak({});
+    } else if (statusJawaban.type == "ukuran") {
+      setPanjang("");
+      setLebar("");
+      setAkar_1("");
+      setAkar_2("");
+      setStatusJawaban({
+        type: "bentuk",
+        isBenar: true,
+      });
+    } else if (statusJawaban.type == "akar") {
+      setAkar_1("");
+      setAkar_2("");
+      setStatusJawaban({
+        type: "bentuk",
+        isBenar: true,
+      });
+    }
     setShowTryAgain(false);
   };
 
-  const screenSize = useScreenSize();
-  let BOX_LEFT =
-    Math.round(screenSize.width / 2 / 32) * 32 -
-    (BIG_BOX_WIDTH + NORMAL_BOX_WIDTH) / 2;
+  const soalSelanjutnya = () => {
+    if (noSoal == LIST_SOAL.length) {
+      alert("Kamu telah menyelesaikan semua soal");
+    } else {
+      setNoSoal((value) => value + 1);
+      setSoal(LIST_SOAL[noSoal]);
+      reset();
+    }
+  };
+
+  const reset = () => {
+    setShowEmoticon(false);
+    setKotak({});
+    setPanjang("");
+    setLebar("");
+    setAkar_1("");
+    setAkar_2("");
+    setStatusJawaban({
+      type: "",
+      isBenar: false,
+    });
+    setShowTryAgain(false);
+    setBentuk("");
+  };
 
   const addBox = (id, title, left, top) => {
     setKotak(
@@ -223,20 +286,28 @@ const Playground = ({ isSnapToGrid }) => {
           </h1>
         </div> */}
         <div
-          className="absolute flex flex-col gap-3 items-center text-black transition-all duration-1000 -translate-y-1/2 p-6 rounded-xl"
+          className="absolute flex flex-row gap-3 items-start text-black transition-all duration-1000 -translate-y-1/2 p-6 rounded-xl"
           style={{
             top: "8rem",
             right: "10%",
           }}
         >
-          <div className="px-3 py-2 bg-white rounded-xl shadow-xl ring-1">
-            Soal
+          <div className="mr-24 tracking-widest flex flex-col items-center gap-6">
+            <p>No</p>
+            <h1 className="text-4xl">
+              {noSoal} / {LIST_SOAL.length}
+            </h1>
           </div>
-          <h1 className="text-3xl">
-            <div className="bg-white rounded-lg px-3 py-1 ring-1 text-3xl font-semibold">
-              <Latex>${SOAL}$</Latex>
+          <div className="flex flex-col gap-3 items-center">
+            <div className="px-3 py-2 bg-white rounded-xl shadow-xl ring-1">
+              Soal
             </div>
-          </h1>
+            <h1 className="text-3xl">
+              <div className="bg-white rounded-lg px-3 py-1 ring-1 text-3xl font-semibold">
+                <Latex>${soal}$</Latex>
+              </div>
+            </h1>
+          </div>
         </div>
 
         <div
@@ -248,7 +319,7 @@ const Playground = ({ isSnapToGrid }) => {
             <button
               className="absolute py-3 px-10 top-0 right-0 bg-red-500 font-bold text-sm rounded-xl  text-white hover:bg-red-600 cursor-pointer transition-all disabled:bg-gray-400 disabled:cursor-not-allowed disabled:ring-0"
               onClick={() => {
-                tryAgain();
+                reset();
               }}
             >
               Reset
@@ -266,7 +337,13 @@ const Playground = ({ isSnapToGrid }) => {
                 {/* VERTIKAL */}
                 <div className="absolute translate-y-24 translate-x-4 w-0.5 h-[22rem] bg-black"></div>
                 <div className="absolute translate-y-24 translate-x-8 h-[22rem] w-[4rem] flex items-center ">
-                  <div className="absolute -rotate-90 -translate-x-36 flex flex-row items-center justify-center gap-2 w-[22rem] h-[4rem]">
+                  <div
+                    className={`absolute -rotate-90 -translate-x-36 flex flex-row items-center justify-center gap-2 w-[22rem] h-[4rem] ${
+                      statusJawaban.type == "ukuran" &&
+                      !statusJawaban.isBenar &&
+                      "text-red-500 text-xl font-bold"
+                    }`}
+                  >
                     <Latex>$Lebar = $</Latex>
                     <Latex>$ {lebar}$</Latex>
                   </div>
@@ -274,69 +351,16 @@ const Playground = ({ isSnapToGrid }) => {
                 {/* HORIZONTAL */}
                 <div className="absolute translate-y-4 translate-x-24 w-[28rem] h-0.5 bg-black"></div>
                 <div className="absolute translate-y-8 translate-x-24 w-[28rem]">
-                  <div className="w-full flex flex-row gap-2 items-center justify-center">
+                  <div
+                    className={`w-full flex flex-row gap-2 items-center justify-center ${
+                      statusJawaban.type == "ukuran" &&
+                      !statusJawaban.isBenar &&
+                      "text-red-500 text-xl font-bold"
+                    }`}
+                  >
                     <Latex>$Panjang = $</Latex>
                     <Latex>$ {panjang}$</Latex>
                   </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div
-            className="absolute text-black transition-all duration-1000"
-            style={{
-              left: step >= 9 ? BIG_BOX_WIDTH * 2 : BOX_LEFT,
-              top: BOX_TOP,
-            }}
-          >
-            <div
-              className="relative animate-popup"
-              style={{ display: preview ? "block" : "none" }}
-            >
-              {/* HORIZONTAL */}
-              <div className="absolute -translate-y-8 w-[128px]">
-                <div className="w-full flex flex-col items-center justify-center">
-                  <Latex>$X$</Latex>
-                  <div className="bg-black translate-y-0 w-3/4 h-[1px]"></div>
-                </div>
-              </div>
-              <div className="absolute w-[64px] translate-x-[128px] -translate-y-8">
-                <div className="w-full flex flex-col items-center justify-center">
-                  <Latex>$1$</Latex>
-                  <div className="bg-black translate-y-0 w-3/4 h-[1px]"></div>
-                </div>
-              </div>
-              {/* HORIZONTAL */}
-              {/* VERTIKAL */}
-              <div className="absolute translate-y-[128px] -translate-x-8 h-[64px] flex items-center">
-                <div className="-rotate-90">
-                  <Latex>$1$</Latex>
-                </div>
-                <div className="bg-black translate-x-4 h-3/4 w-[1px]"></div>
-              </div>
-              <div className="absolute -translate-x-8 h-32 flex items-center">
-                <div className="-rotate-90">
-                  <Latex>$X$</Latex>
-                </div>
-                <div className="bg-black translate-x-1 h-3/4 w-[1px]"></div>
-              </div>
-              {/* VERTIKAL */}
-            </div>
-            <div
-              className="relative animate-popup"
-              style={{ display: previewPL ? "block" : "none" }}
-            >
-              {/* HORIZONTAL */}
-              <div className="absolute -translate-y-8 w-[192px]">
-                <div className="w-full flex flex-col items-center justify-center">
-                  <Latex>$P = X + 1$</Latex>
-                </div>
-              </div>
-
-              {/* VERTIKAL */}
-              <div className="absolute -translate-x-16 h-48 flex items-center">
-                <div className="-rotate-90 w-max">
-                  <Latex>$L = X + 1$</Latex>
                 </div>
               </div>
             </div>
@@ -367,6 +391,7 @@ const Playground = ({ isSnapToGrid }) => {
                     <Latex className="pointer-events-none">${panjang} $</Latex>
                     <input
                       type="text"
+                      value={panjang}
                       className={`absolute left-0 top-0 px-3 py-1 z-10 w-24 bg-transparent focus:outline-none ${
                         panjang.length > 0
                           ? "text-transparent w-fit tracking-widest text-2xl"
@@ -382,9 +407,10 @@ const Playground = ({ isSnapToGrid }) => {
                     <Latex>${lebar} $</Latex>
                     <input
                       type="text"
+                      value={lebar}
                       className={`absolute left-0 top-0 pl-3 pr-3 py-1 z-10 w-24 bg-transparent focus:outline-none ${
                         lebar.length > 0
-                          ? "text-transparent w-8"
+                          ? "text-transparent w-fit tracking-widest text-2xl"
                           : "text-black w-8"
                       }`}
                       onChange={(e) => setLebar(e.target.value)}
@@ -395,6 +421,7 @@ const Playground = ({ isSnapToGrid }) => {
               <select
                 name="bentuk"
                 id="bentuk"
+                value={bentuk}
                 className="p-2 bg-white rounded-lg ring-1 text-lg w-48"
                 onChange={(e) => setBentuk(e.target.value)}
               >
@@ -408,7 +435,7 @@ const Playground = ({ isSnapToGrid }) => {
                 className=" items-center gap-2 text-sm font-medium animate-popup"
                 style={{ display: bentuk != "" ? "flex" : "none" }}
               >
-                Luas {bentuk == "persegi" ? "Persegi" : "Persegi Panjang"} =
+                Luas =
                 <div className="bg-white text-sm rounded-lg px-3 py-1 ring-1">
                   <Latex>
                     ${bentuk == "persegi" ? "sisi.sisi" : "panjang.lebar"}$
@@ -453,8 +480,11 @@ const Playground = ({ isSnapToGrid }) => {
                       <Latex>${akar_1} $</Latex>
                       <input
                         type="text"
+                        value={akar_1}
                         className={`absolute left-0 top-0 px-3 py-1 z-10 w-24 bg-transparent focus:outline-none ${
-                          akar_1.length > 0 ? "text-transparent w-8" : "w-8"
+                          akar_1.length > 0
+                            ? "text-transparent w-fit tracking-widest text-2xl"
+                            : "w-8"
                         }`}
                         onChange={(e) => setAkar_1(e.target.value)}
                       />
@@ -470,8 +500,11 @@ const Playground = ({ isSnapToGrid }) => {
                       <Latex>${akar_2} $</Latex>
                       <input
                         type="text"
+                        value={akar_2}
                         className={`absolute left-0 top-0 px-3 py-1 z-10 w-24 bg-transparent focus:outline-none ${
-                          akar_2.length > 0 ? "text-transparent w-8" : "w-8"
+                          akar_2.length > 0
+                            ? "text-transparent w-fit tracking-widest text-2xl"
+                            : "w-8"
                         }`}
                         onChange={(e) => setAkar_2(e.target.value)}
                       />
@@ -515,27 +548,38 @@ const Playground = ({ isSnapToGrid }) => {
         {showEmoticon && (
           <div className="absolute top-8 right-96 rounded-xl text-white cursor-pointer transition-all disabled:bg-gray-400 disabled:cursor-not-allowed disabled:ring-0">
             <div className="relative">
-              <div className="absolute -top-32 right-0 p-3 h-fit w-64 rounded-lg text-lg ring-1 ring-slate-300 bg-white text-black animate-popup transition-all">
-                {isBentukBenar
-                  ? "Kerja Bagus, bentuk yang kamu buat sudah benar, sekarang hitung akarnya!"
-                  : "Bentuk yang kamu buat masih salah, coba ulang kembali"}
-              </div>
+              {statusJawaban.isBenar == false && (
+                <div className="absolute -top-32 right-0 p-3 h-fit w-64 rounded-lg text-lg ring-1 ring-slate-300 bg-white text-black animate-popup transition-all">
+                  {statusJawaban.type == "bentuk"
+                    ? "Bentuk yang kamu buat masih salah, coba ulang kembali"
+                    : statusJawaban.type == "ukuran"
+                    ? "Panjang atau Lebar masih salah, coba hitung kembali"
+                    : "Himpunan penyelesaian masih salah, coba hitung kembali"}
+                </div>
+              )}
+              {statusJawaban.isBenar == true && (
+                <div className="absolute -top-32 right-0 p-3 h-fit w-64 rounded-lg text-lg ring-1 ring-slate-300 bg-white text-black animate-popup transition-all">
+                  {statusJawaban.type == "bentuk"
+                    ? "Kerja Bagus, bentuk yang kamu buat sudah benar, sekarang hitung panjang lebarnya!"
+                    : statusJawaban.type == "akar" &&
+                      "Kerja Bagus, Himpunan penyelesaian sudah benar, sekarang lanjut ke soal selanjutnya!"}
+                </div>
+              )}
             </div>
             <div className="font-bold text-6xl animate-popup transition-all">
-              {isBentukBenar ? "🥳" : "😢"}
+              {statusJawaban.isBenar ? "🥳" : "😢"}
             </div>
           </div>
         )}
-        {isBentukBenar ? (
-          isAkarBenar ? (
+        {statusJawaban.isBenar == true ? (
+          statusJawaban.type == "akar" ? (
             <button
               className="absolute top-8 right-32 bg-blue-500 font-bold text-lg rounded-xl py-6 px-12  text-white hover:bg-blue-600 cursor-pointer transition-all disabled:bg-gray-400 disabled:cursor-not-allowed disabled:ring-0"
               onClick={() => {
-                checkAnswer();
+                soalSelanjutnya();
               }}
-              disabled={!isAkarBenar}
             >
-              Lanjut
+              {noSoal == LIST_SOAL.length ? "Selesai" : "Lanjut"}
             </button>
           ) : (
             <button
@@ -543,7 +587,7 @@ const Playground = ({ isSnapToGrid }) => {
               onClick={() => {
                 checkPerhitungan();
               }}
-              disabled={akar_1 != "" && akar_2 != ""}
+              disabled={akar_1 == "" && akar_2 == ""}
             >
               Cek Akar
             </button>
